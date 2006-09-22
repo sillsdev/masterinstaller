aacec347e832a7bce2b3027f5bf5d7bcb2e8bbab
imrie <alistair_imrie@sil.org>
2006-09-22 16:16:11 +0000

    Extended to include Encoding Converters installer updates, and to be able to upload compressed CD image to more than one server.

--- Utils/FwIsoBuildAndUpload.js
+++ Utils/FwIsoBuildAndUpload.js
@@ -7,38 +7,53 @@
 // 4) Must have CdImage.js script in the same folder as this file.
 //
 // Call this script with the following parameters:
-// 1) The path where up-to-date SetupFW.msi is located
-// 2) The age limit, in hours, for the msi file to be accepted
-// 3) The path where the ISO and self-extracting archive are to be built
-// 4) The folder in the above path (3) where the CD files and folders are laid out
-// 5) The destination where the self-extracting archive is to be uploaded
-// Note that the paths in (1), (3) and (5) can all be network paths. FTP sites are not allowed.
+// 1) The path where up-to-date FieldWorks installer files are located
+// 2) The path where up-to-date Encoding Converters installer files are located
+// 3) The age limit, in hours, for the files to be accepted
+// 4) The path where the ISO and self-extracting archive are to be built
+// 5) The folder in the above path (3) where the CD files and folders are laid out
+// 6 and beyond) Destination(s) where the self-extracting archive is to be uploaded
+// Note that the paths in (1), (2), (4) and (6) can all be network paths. FTP sites are not allowed.
+//
+// Example parameters:
+// 1) "\\cana\LSDev\Installers\FW4.0"
+// 2) "\\cana\LSDev\Installers\EC2.5"
+// 3) 24
+// 4) "E:\CD Builder\CDs\FW 4.0"
+// 5) LST_07-011
+// 6) "\\jar-file\sillangsoft\FW_4_0_ISO"
+// 7) "\\cana\LSDev\Installers\FW4.0"
 //
 // The program works as follows:
-// 1) Check existence of SetupFW.msi in path argument (1).
-// 2) If msi file is young enough (argument 2), copy it into CD file structure (arg 3+4)
+// 1) Check existence of files in path arguments (1) and (2).
+// 2) If files are young enough (argument 3), copy them into CD file structure (arg 4+5)
 // 3) Make ISO file from CD file structure.
 // 4) Compress ISO file into self-extracting archive.
-// 5) Upload archive to destination in argument (5).
+// 5) Upload archive to destination in arguments (6) and beyond.
 
 // Check we have valid arguments:
-if (WScript.Arguments.Length != 5)
+if (WScript.Arguments.Length < 6)
 {
-	WScript.Echo("Usage: needs 5 arguments:\n" +
-		"1) The path where up-to-date SetupFW.msi is located\n" +
-		"2) The age limit, in hours, for the msi files to be accepted\n" +
-		"3) The path where the ISO and self-extracting archive are to be built\n" +
-		"4) The folder in the above path (3) where the CD files and folders are laid out\n" +
-		"5) The destination where the self-extracting archive is to be uploaded\n" +
-		"Note that the paths in (1), (3) and (5) can all be network paths. FTP sites are not allowed.");
+	WScript.Echo("Usage: needs at least 6 arguments:\n" +
+		"1) The path where up-to-date FW installer is located\n" +
+		"2) The path where up-to-date EC installer is located\n" +
+		"3) The age limit, in hours, for the msi files to be accepted\n" +
+		"4) The path where the ISO and self-extracting archive are to be built\n" +
+		"5) The folder in the above path (3) where the CD files and folders are laid out\n" +
+		"6 and beyond) Destination(s) where the self-extracting archive is to be uploaded\n" +
+		"Note that the paths in (1), (2), (4) and (6) can all be network paths. FTP sites are not allowed.");
 	WScript.Quit();
 }
 
-var MsiSourcePath = WScript.Arguments.Item(0);
-var AgeLimitHours = WScript.Arguments.Item(1);
-var WorkspacePath = WScript.Arguments.Item(2);
-var CdFolder = WScript.Arguments.Item(3);
-var DestinationPath = WScript.Arguments.Item(4);
+var FWSourcePath = WScript.Arguments.Item(0);
+var ECSourcePath = WScript.Arguments.Item(1);
+var NumECCabFiles = 13;
+var AgeLimitHours = WScript.Arguments.Item(2);
+var WorkspacePath = WScript.Arguments.Item(3);
+var CdFolder = WScript.Arguments.Item(4);
+var DestinationPath = new Array();
+for (i = 5; i < WScript.Arguments.length; i++)
+	DestinationPath.push(WScript.Arguments.Item(i));
 
 var fso = new ActiveXObject("Scripting.FileSystemObject");	
 var shellObj = new ActiveXObject("WScript.Shell");
@@ -59,24 +74,41 @@ var ArchiveFile = IsoFile + ".exe";
 var ArchivePath = fso.BuildPath(WorkspacePath, ArchiveFile);
 var CdFullPath = fso.BuildPath(WorkspacePath, CdFolder);
 
-// Check if installer exists on MsiSourcePath:
-var SetupFwMsiSource = fso.BuildPath(MsiSourcePath, "SetupFW.msi");
+// Check if installer exists on FWSourcePath:
+var SetupFwMsiSource = fso.BuildPath(FWSourcePath, "SetupFW.msi");
 var SetupFwMsi = fso.GetFile(SetupFwMsiSource);
 if (SetupFwMsi == null)
-	LogFatalError("SetupFW.msi not found in " + MsiSourcePath);
+	LogFatalError("SetupFW.msi not found in " + FWSourcePath);
 
-var SetupFw5CabSource = fso.BuildPath(MsiSourcePath, "SetupFW5.cab");
+var SetupFw5CabSource = fso.BuildPath(FWSourcePath, "SetupFW5.cab");
 var SetupFw5Cab = fso.GetFile(SetupFw5CabSource);
 if (SetupFw5Cab == null)
-	LogFatalError("SetupFw5.Cab not found in " + MsiSourcePath);
+	LogFatalError("SetupFw5.Cab not found in " + FWSourcePath);
 
-var SetupFw6CabSource = fso.BuildPath(MsiSourcePath, "SetupFW6.cab");
+var SetupFw6CabSource = fso.BuildPath(FWSourcePath, "SetupFW6.cab");
 var SetupFw6Cab = fso.GetFile(SetupFw6CabSource);
 if (SetupFw6Cab == null)
-	LogFatalError("SetupFw6.Cab not found in " + MsiSourcePath);
+	LogFatalError("SetupFw6.Cab not found in " + FWSourcePath);
 
-// Get date of installer:
+// Check if installer exists on ECSourcePath:
+var SetupECMsiSource = fso.BuildPath(ECSourcePath, "SetupEC.msi");
+var SetupECMsi = fso.GetFile(SetupECMsiSource);
+if (SetupECMsi == null)
+	LogFatalError("SetupEC.msi not found in " + ECSourcePath);
+
+var SetupECCabSource = new Array();
+var SetupECCab = new Array();
+for (i = 0; i < NumECCabFiles; i++)
+{
+	SetupECCabSource.push(fso.BuildPath(ECSourcePath, "SetupEC" + (i + 1) + ".cab"));
+	SetupECCab.push(fso.GetFile(SetupECCabSource[i]));
+	if (SetupECCab[i] == null)
+		LogFatalError("SetupEC" + (i + 1) + ".cab not found in " + ECSourcePath);
+}
+
+// Get date of installer files:
 var SetupFwMsiDateMs = Date.parse(SetupFwMsi.DateLastModified);
+var SetupECMsiDateMs = Date.parse(SetupECMsi.DateLastModified);
 
 // See if installer date is within AgeLimitHours hours of now:
 var Now = new Date();
@@ -85,7 +117,12 @@ var SetupFwMsiAgeHours = SetupFwMsiAgeMs / (1000 * 60 * 60);
 if (SetupFwMsiAgeHours > AgeLimitHours)
 	LogFatalError("SetupFW.msi more than " + AgeLimitHours + " hours old (" + SetupFwMsiAgeHours + " hours)");
 
-// Copy installer from MsiSourcePath to CdFolder:
+var SetupECMsiAgeMs = Now.getTime() - SetupECMsiDateMs;
+var SetupECMsiAgeHours = SetupECMsiAgeMs / (1000 * 60 * 60);
+if (SetupECMsiAgeHours > AgeLimitHours)
+	LogFatalError("SetupEC.msi more than " + AgeLimitHours + " hours old (" + SetupECMsiAgeHours + " hours)");
+
+// Copy FW installer files from FWSourcePath to CdFolder:
 var LocalFwPath = fso.BuildPath(CdFullPath, "FieldWorks");
 var Cmd = '"' + BCopy + '" "' + SetupFwMsiSource + '" "' + LocalFwPath + '" /b:500 /r:40 /f';
 shellObj.Run(Cmd, 1, true);
@@ -95,6 +132,16 @@ shellObj.Run(Cmd, 1, true);
 
 Cmd = '"' + BCopy + '" "' + SetupFw6CabSource + '" "' + LocalFwPath + '" /b:500 /r:40 /f';
 shellObj.Run(Cmd, 1, true);
+
+// Copy EC installer files from ECSourcePath to CdFolder:
+var LocalECPath = fso.BuildPath(CdFullPath, "Encoding Converters");
+Cmd = '"' + BCopy + '" "' + SetupECMsiSource + '" "' + LocalECPath + '" /b:500 /r:40 /f';
+shellObj.Run(Cmd, 1, true);
+for (i = 0; i < NumECCabFiles; i++)
+{
+	Cmd = '"' + BCopy + '" "' + SetupECCabSource[i] + '" "' + LocalECPath + '" /b:500 /r:40 /f';
+	shellObj.Run(Cmd, 1, true);
+}
 
 // Remove existing ISO file:
 if (fso.FileExists(IsoPath))
@@ -124,9 +171,12 @@ Cmd = 'cmd /D /C ""' + BatchFile + '""';
 shellObj.Run(Cmd, 1, true);
 fso.DeleteFile(BatchFile);
 
-// Copy file to DestinationPath:
-Cmd = '"' + BCopy + '" "' + ArchivePath + '" "' + fso.BuildPath(DestinationPath, ArchiveFile) + '" /b:500 /r:40 /f';
-shellObj.Run(Cmd, 1, true);
+// Copy file to DestinationPath(s):
+for (i = 0; i < DestinationPath.length; i++)
+{
+	Cmd = '"' + BCopy + '" "' + ArchivePath + '" "' + fso.BuildPath(DestinationPath[i], ArchiveFile) + '" /b:500 /r:40 /f';
+	shellObj.Run(Cmd, 1, true);
+}
 
 
 function LogFatalError(Msg)

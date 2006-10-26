48abd72565e7b3a3c3ecb630cf7b36d326074cf6
imrie <alistair_imrie@sil.org>
2006-10-26 21:11:46 +0000

    Updated Toolbox from 1.4 to 1.5.0

---
+++ TestToolbox15Presence.cpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <tchar.h>
+
+// Crude test of Toolbox 1.5 installation
+bool TestToolbox15Presence(const TCHAR * /*pszMinVersion*/, const TCHAR * /*pszMaxVersion*/,
+						   const TCHAR * /*pszCriticalFile*/)
+{
+	bool fResult = false;
+	LONG lResult;
+	HKEY hKey = NULL;
+	const TCHAR * pszTemplateDisplayName = _T("Toolbox 1.5");
+
+	lResult = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+		_T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Toolbox_is1"),
+		NULL, KEY_READ, &hKey);
+
+	// Read display name:
+	if (ERROR_SUCCESS == lResult)
+	{
+		_TCHAR * pszDisplayName = NULL;
+		DWORD cbData = 0;
+
+		// Find out how much space we need to hold display name:
+		lResult = RegQueryValueEx(hKey, _T("DisplayName"), NULL, NULL, NULL, &cbData);
+		if (ERROR_SUCCESS == lResult)
+		{
+			pszDisplayName = new TCHAR [cbData];
+			RegQueryValueEx(hKey, _T("DisplayName"), NULL, NULL, (LPBYTE)pszDisplayName, &cbData);
+
+			if (ERROR_SUCCESS == lResult)
+			{
+				if (_strnicmp(pszDisplayName, pszTemplateDisplayName, strlen(pszTemplateDisplayName)) == 0)
+					fResult = true;
+			}
+
+			delete[] pszDisplayName;
+			pszDisplayName = NULL;
+		}
+	}
+
+	RegCloseKey(hKey);
+
+	return fResult;
+}

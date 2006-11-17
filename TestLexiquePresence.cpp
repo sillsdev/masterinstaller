93e231bdf2e56b89ee43eabff63b34d6331cc9fd
imrie <alistair_imrie@sil.org>
2006-11-17 15:53:46 +0000

    Updated Lexique Pro from version 2.4.6 to version 2.6.6.

--- TestLexiquePresence.cpp
+++ TestLexiquePresence.cpp
@@ -2,19 +2,31 @@
 
 #include <tchar.h>
 
-// Crude test of Lexique Pro installation
+// Crude test of Lexique Pro installation - currently only recognizes Lexique Pro 2.6.6
 bool TestLexiquePresence(const TCHAR * /*pszMinVersion*/, const TCHAR * /*pszMaxVersion*/,
 						 const TCHAR * /*pszCriticalFile*/)
 {
 	bool fResult = false;
-	LONG lResult;
 	HKEY hKey = NULL;
+	const TCHAR * pszTemplateDisplayName = _T("Lexique Pro 2.6.6");
 
-	lResult = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+	if (ERROR_SUCCESS == RegOpenKeyEx(HKEY_LOCAL_MACHINE,
 		_T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Lexique Pro - Bambara_is1"),
-		NULL, KEY_READ, &hKey);
+		NULL, KEY_READ, &hKey))
+	{
+		DWORD cchAppName = 0;
+		TCHAR * pszAppName = NULL;
+		const TCHAR * pszAppValue = _T("DisplayName");
 
-	RegCloseKey(hKey);
+		if (ERROR_SUCCESS == RegQueryValueEx(hKey, pszAppValue, NULL, NULL, NULL, &cchAppName))
+		{
+			pszAppName = new TCHAR [cchAppName];
+			RegQueryValueEx(hKey, pszAppValue, NULL, NULL, (LPBYTE)pszAppName, &cchAppName);
 
-	return (ERROR_SUCCESS == lResult);
+			if (_tcscmp(pszAppName, pszTemplateDisplayName) == 0)
+				fResult = true;
+		}
+		RegCloseKey(hKey);
+	}
+	return fResult;
 }

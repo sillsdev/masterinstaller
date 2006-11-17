70ebc161e78eeebcb9ff0238d62d88e70d601a29
imrie <alistair_imrie@sil.org>
2006-11-17 17:26:27 +0000

    Updated Carla Studio to version 2.9.0.3.

--- TestCarlaStudioPresence.cpp
+++ TestCarlaStudioPresence.cpp
@@ -2,19 +2,77 @@
 
 #include <tchar.h>
 
-// Crude test of Carla Studio installation
-bool TestCarlaStudioPresence(const TCHAR * /*pszMinVersion*/, const TCHAR * /*pszMaxVersion*/,
+// Test for Carla Studio installation. Uses the path in the registry for product uninstallation
+// to get the version of the main .exe file
+bool TestCarlaStudioPresence(const TCHAR * pszMinVersion, const TCHAR * pszMaxVersion,
 							 const TCHAR * /*pszCriticalFile*/)
 {
 	bool fResult = false;
-	LONG lResult;
 	HKEY hKey = NULL;
 
-	lResult = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+	if (ERROR_SUCCESS == RegOpenKeyEx(HKEY_LOCAL_MACHINE,
 		_T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\CarlaStudio"),
-		NULL, KEY_READ, &hKey);
+		NULL, KEY_READ, &hKey))
+	{
+		// Get the uninstall string:
+		DWORD cchUninstallString = 0;
+		TCHAR * pszUninstallString = NULL;
+		const TCHAR * pszUninstallStringValue = _T("UninstallString");
 
-	RegCloseKey(hKey);
+		if (ERROR_SUCCESS == RegQueryValueEx(hKey, pszUninstallStringValue, NULL, NULL, NULL,
+			&cchUninstallString))
+		{
+			pszUninstallString = new TCHAR [cchUninstallString];
+			if (ERROR_SUCCESS == RegQueryValueEx(hKey, pszUninstallStringValue, NULL, NULL,
+				(LPBYTE)pszUninstallString, &cchUninstallString))
+			{
+				// Parse the UninstallString to get the installation path. It typically looks like this:
+				// C:\WINDOWS\IsUninst.exe -f"C:\Program Files\SIL\CarlaStudio\Uninst.isu"
+				const TCHAR * pszFlag = _T("-f");
+				TCHAR * pchFlagStart = _tcsstr(pszUninstallString, pszFlag);
+				if (pchFlagStart)
+				{
+					TCHAR * pszInstallPath = pchFlagStart + _tcslen(pszFlag);
+					// Check for a quoted string - bypass quote marks if present:
+					if (_tcsnccmp(pszInstallPath, _T("\""), 1) == 0)
+					{
+						pszInstallPath++;
+						size_t cch = _tcslen(pszInstallPath) - 1;
+						if (_tcscmp(&pszInstallPath[cch], _T("\"")) == 0)
+							pszInstallPath[cch] = 0;
+					}
+					// Now find main .exe file and get its version:
+					TCHAR * pchLastBackslash = _tcsrchr(pszInstallPath, '\\');
+					if (pchLastBackslash)
+					{
+						*pchLastBackslash = 0;
+						TCHAR * pszMainExePath = new_sprintf(_T("%s\\CStudio.exe"), pszInstallPath);
 
-	return (ERROR_SUCCESS == lResult);
+						// See if the file exists and what version it is:
+						__int64 nVersion;
+						bool fFound = GetFileVersion(pszMainExePath, nVersion);
+						delete[] pszMainExePath;
+						pszMainExePath = NULL;
+
+						if (fFound)
+						{
+#if !defined NOLOGGING
+							TCHAR * pszVersion = GenVersionText(nVersion);
+							g_Log.Write(_T("Found Carla Studio version %s"), pszVersion);
+							delete[] pszVersion;
+							pszVersion = NULL;
+#endif
+
+							fResult = VersionInRange(nVersion, pszMinVersion, pszMaxVersion);
+						}
+					}
+				}
+			}
+			delete[] pszUninstallString;
+			pszUninstallString = NULL;
+		}
+		RegCloseKey(hKey);
+	}
+
+	return (fResult);
 }

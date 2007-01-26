a553fc7cec12e7fc2cab9d3d48a05d83a55abdf0
imrie <alistair_imrie@sil.org>
2007-01-26 21:25:21 +0000

    Corrected erroneous reference to old MSDE test.

--- TestIfSqlServerUpgradeNeeded.cpp
+++ TestIfSqlServerUpgradeNeeded.cpp
@@ -8,7 +8,7 @@ bool g_fSqlServerUpgradeNeeded = false;
 // Check if an older version of MSDE (SILFW) is present (anything before version 9 - SQL Server 2005).
 int TestIfSqlServerUpgradeNeeded(const TCHAR * /*pszCriticalFile*/)
 {
-	if (TestMsdeSILFWPresence(NULL, _T("8.9999"), NULL))
+	if (TestSqlSILFWPresence(NULL, _T("8.9999"), NULL))
 		g_fSqlServerUpgradeNeeded = true;
 
 	return 0;

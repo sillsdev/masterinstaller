642340fc1b4e48f4183486e00a41c0dc88233be6
imrie <alistair_imrie@sil.org>
2007-03-16 21:35:46 +0000

    Changed KeymanKeyboardsInstaller.cpp to request reboot on return.

--- KeymanKeyboardsInstaller.cpp
+++ KeymanKeyboardsInstaller.cpp
@@ -2,5 +2,8 @@
 
 DWORD KeymanKeyboardsInstaller(bool /*fFlag*/, const TCHAR * pszCriticalFile)
 {
-	return KeymanFunction(false, pszCriticalFile);
+	DWORD retVal = KeymanFunction(false, pszCriticalFile);
+	if (retVal == 0)
+		return ERROR_SUCCESS_REBOOT_REQUIRED; // Keyboards are not effective till after a reboot.
+	return retVal;
 }

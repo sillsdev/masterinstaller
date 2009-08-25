ed0431fbdfd5ee04d5a4fe891dd5e6ea2241a93a
imrie <alistair_imrie@sil.org>
2009-08-25 16:17:11 +0000

    Added Shoebox Utilities.

---
+++ TestShoeboxUtilsPresence.cpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <tchar.h>
+
+// Crude test of Shoebox Utilities installation
+bool TestShoeboxUtilsPresence(const _TCHAR * /*pszMinVersion*/, const _TCHAR * /*pszMaxVersion*/,
+						   SoftwareProduct * /*Product*/)
+{
+	bool fResult = false;
+
+	_TCHAR * pszDisplayName = NewRegString(HKEY_LOCAL_MACHINE,
+		_T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\SIL Shoebox Utilities_is1"),
+		_T("DisplayName"));
+
+	if (pszDisplayName)
+	{
+		const TCHAR * pszTemplateDisplayName = _T("SIL Shoebox Utilities_is1");
+
+		if (_tcsnicmp(pszDisplayName, pszTemplateDisplayName,
+					_tcslen(pszTemplateDisplayName)) == 0)
+		{
+			fResult = true;
+		}
+		delete[] pszDisplayName;
+		pszDisplayName = NULL;
+	}
+	return fResult;
+}

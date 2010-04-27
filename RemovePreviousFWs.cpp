d3e6a7e4c59c1d7dc64f2263136192e96d89af2e
imrie <alistair_imrie@sil.org>
2010-04-27 18:27:10 +0000

    Newly added master installers for FW 6.0.4 BTE and SE.

--- RemovePreviousFWs.cpp
+++ RemovePreviousFWs.cpp
@@ -16,7 +16,7 @@ int RemovePreviousFWs(SoftwareProduct * /*Product*/)
 	{
 		_T("{E161291A-BD0C-46F6-9F39-6B165BDD4CCC}"), // FW 0.8.0, 1.3 and 1.4 ("Aussie", Disks 3 and 4)
 		_T("{2D424459-8B40-41F3-94F7-4D02BFAC39E7}"), // FW 2.0 (Disk 5)
-		_T("{D8D2DBAB-7487-4A6E-B369-7F1932B3BDFE}"), // old WW installer for Harvwest alpha
+		_T("{D8D2DBAB-7487-4A6E-B369-7F1932B3BDFE}"), // old WW installer for Harvest alpha
 		_T("{59EDA034-26E5-4B7A-9924-335BA200B461}"), // FW 3.0
 		_T("{823B00C2-18DB-451B-93F6-068E96A90BFD}"), // FW 3.1
 		_T("{DB19E5F7-187F-4E62-8D41-BA67C63BE88E}"), // After FW 3.1 but before the switch to VS 2005
@@ -37,7 +37,8 @@ int RemovePreviousFWs(SoftwareProduct * /*Product*/)
 		_T("{3B831BB9-756E-49be-BFEC-83097C11EDB9}"), // FW 6.0
 		_T("{A403C1D2-7ABD-4a53-981D-C6C0F6EF6AA4}"), // FW 6.0.1
 		_T("{A7FD1340-2DFC-4072-B130-3363AB0B0A57}"), // FW 6.0.2
-		//_T("{8F8C0439-8E0F-4f74-BED6-DBD7FB05731C}"), // FW 6.0.3
+		_T("{8F8C0439-8E0F-4f74-BED6-DBD7FB05731C}"), // FW 6.0.3
+		//_T("{2A458F3A-B88E-416f-997E-F7935489C054}"), // FW 6.0.4
 	};
 	for (int i = 0; i < (sizeof(pszProductCode) / sizeof(pszProductCode[0])); i++)
 	{

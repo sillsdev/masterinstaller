52b09565c04f76ff58cd68f3b53cdf2b7f9995a3
imrie <alistair_imrie@sil.org>
2010-12-22 15:45:31 +0000

    Created new master installer for FW 7 Beta which does not attempt to include HotFixes for .NET 3.5 SP1.

---
+++ DotNet35SP1PostInstall.cpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <tchar.h>
+#include <shlobj.h>
+#include "msi.h"
+
+int DotNet35SP1PostInstall(SoftwareProduct * /* Product */)
+{
+	MessageBox(NULL,
+		_T("WARNING: Microsoft has released several updates (known as HotFixes) to the .NET 3.5 framework. You are strongly advised to use the Windows Update service to download and install them."),
+		_T("Updates are required to your system"), MB_ICONSTOP | MB_OK);
+
+	return 0;
+}
+

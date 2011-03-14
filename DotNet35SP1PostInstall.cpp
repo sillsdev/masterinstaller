06e8d3e43edc99b7c22bd2ebbb7c488c25d04aa3
imrie <alistair_imrie@sil.org>
2011-03-14 18:50:12 +0000

    Update to warning message after installing .NET 3.5 SP1

--- DotNet35SP1PostInstall.cpp
+++ DotNet35SP1PostInstall.cpp
@@ -7,7 +7,7 @@
 int DotNet35SP1PostInstall(SoftwareProduct * /* Product */)
 {
 	MessageBox(NULL,
-		_T("WARNING: Microsoft has released several updates (known as HotFixes) to the .NET 3.5 framework. You are strongly advised to use the Windows Update service to download and install them."),
+		_T("WARNING: Microsoft has released several updates (known as HotFixes) to the .NET 3.5 framework. After completing the installation of FieldWorks, you are strongly advised to use the Windows Update service to download and install them."),
 		_T("Updates are required to your system"), MB_ICONSTOP | MB_OK);
 
 	return 0;

59f71e408899ea2b80d81b4f4fd12015b46bdd4a
imrie <alistair_imrie@sil.org>
2012-08-06 17:03:01 +0000

    Removed obsolete TestPresence Web Control

--- Code and Projects/TestPresence Web Control/Release/BuildAndSignCab.bat
+++
@@ -1,3 +0,0 @@
-del TestPresence.cab
-cabarc -s 6144 N TestPresence.cab TestPresence.ocx TestPresence.inf
-..\..\Utils\Sign\signcode -n "SIL Software Evaluator" -spc D:\Comodo.spc -v D:\Comodo.pvk -t http://timestamp.comodoca.com/authenticode TestPresence.cab

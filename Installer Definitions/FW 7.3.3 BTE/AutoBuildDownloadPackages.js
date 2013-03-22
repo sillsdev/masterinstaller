8389fc69f59395eab95682d61ecf079996854c22
imrie <alistair_imrie@sil.org>
2013-03-22 20:52:17 +0000

    Release of FW 7.3.3

---
+++ Installer Definitions/FW 7.3.3 BTE/AutoBuildDownloadPackages.js
@@ -0,0 +1,41 @@
+// Fills in details for download packages automatically.
+// This instance created AUTOMATICALLY during a previous run.
+function AutomatePackages()
+{
+	SetElement("FlavorName1", "FW733_BTE_A");
+	SetElement("FlavorUrl1", "http://downloads.sil.org/FieldWorks/7.3.3/BTE/FW733_BTE_A.exe");
+	AddFlavor();
+	SetElement("FlavorName2", "FW733_BTE_B");
+	SetElement("FlavorUrl2", "http://downloads.sil.org/FieldWorks/7.3.3/BTE/FW733_BTE_B.exe");
+
+	NextStage();
+
+	SelectElement("IncludedF1P1", true);
+	SelectElement("IncludedF1P2", false);
+	SelectElement("IncludedF1P3", false);
+	SelectElement("IncludedF1P4", false);
+	SelectElement("IncludedF1P5", true);
+	SelectElement("IncludedF1P6", false);
+	SelectElement("IncludedF1P7", false);
+	SelectElement("IncludedF1P8", true);
+
+	SelectElement("IncludedF2P1", true);
+	SelectElement("IncludedF2P2", false);
+	SelectElement("IncludedF2P3", false);
+	SelectElement("IncludedF2P4", false);
+	SelectElement("IncludedF2P5", false);
+	SelectElement("IncludedF2P6", false);
+	SelectElement("IncludedF2P7", false);
+	SelectElement("IncludedF2P8", true);
+
+	NextStage();
+
+	SetElement("OutputPath", "G:\\Software Package Builder\\Web Downloads\\FW 7.3.3 BTE");
+	SelectElement("WriteXml", true);
+	SelectElement("WriteDownloadsXml", true);
+	SelectElement("Compile", true);
+	SelectElement("GatherFiles", true);
+	SelectElement("BuildSfx", true);
+	SetElement("SfxStyle", "UseFwSfx");
+	SetElement("SaveSettings", false);
+}

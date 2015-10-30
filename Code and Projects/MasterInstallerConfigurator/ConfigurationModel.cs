2e363fdc7b7299dcad765da4f006b5fb7f056b0d
Jason Naylor <jason_naylor@sil.org>
2015-10-30 14:34:57 -0500

    Start adding the model options and tests
    * Add Basic Model Reading and writing tests
    * Handle the AutoConfigure options

--- Code and Projects/MasterInstallerConfigurator/ConfigurationModel.cs
+++ Code and Projects/MasterInstallerConfigurator/ConfigurationModel.cs
@@ -1,9 +1,59 @@
-﻿using System.Xml.Serialization;
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace MasterInstallerConfigurator
 {
 	[XmlRootAttribute("MasterInstaller", IsNullable = false)]
-	class ConfigurationModel
+	public class ConfigurationModel
 	{
+		public SetupOptions AutoConfigure { get; set; }
+
+		public GeneralOptions General { get; set; }
+
+		public List<Product> Products { get; set; }
+
+
+		public class SetupOptions
+		{
+			[XmlElement(ElementName = "CppFilePath")]
+			public string CppPath { get; set; }
+
+			[XmlElement(ElementName = "ProductsPath")]
+			public string ProductsPath { get; set; }
+
+			[XmlElement(ElementName = "CDsPath")]
+			public string CdPath { get; set; }
+
+			[XmlElement(ElementName = "ExternalHelpSource")]
+			public string HelpSource { get; set; }
+
+			[XmlElement(ElementName = "ExternalHelpDestination")]
+			public string HelpDest { get; set; }
+
+			[XmlElement(ElementName = "TermsOfUseSource")]
+			public string TermsSource { get; set; }
+
+			[XmlElement(ElementName = "TermsOfUseDestination")]
+			public string TermsDest { get; set; }
+
+			[XmlElement(ElementName = "CdImagePath")]
+			public string ImagePath { get; set; }
+
+			[XmlElement(ElementName = "CdSize")]
+			public int CdSize { get; set; }
+
+			[XmlElement(ElementName = "MinFileSize")]
+			public int MinFileSize { get; set; }
+		}
+
+		public class Product
+		{
+			public object AutoConfigure { get; set; }
+		}
+
+		public class GeneralOptions
+		{
+			public string Title { get; set; }
+		}
 	}
 }
--- Code and Projects/MasterInstallerConfiguratorTests/MasterInstallerConfiguratorTests/ConfigurationModelTests.cs
+++ Code and Projects/MasterInstallerConfiguratorTests/MasterInstallerConfiguratorTests/ConfigurationModelTests.cs
@@ -1,14 +1,84 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.IO;
+using System.Xml.Serialization;
+using MasterInstallerConfigurator;
+using NUnit.Framework;
 
 namespace MasterInstallerConfiguratorTests
 {
-	[TestClass]
+	[TestFixture]
 	public class ConfigurationModelTests
 	{
-		[TestMethod]
-		public void TestMethod1()
+		[Test]
+		public void CanPersistBasicModel()
 		{
+			var model = new ConfigurationModel();
+			var serializer = new XmlSerializer(typeof(ConfigurationModel));
+			using (var textWriter = new StringWriter())
+			{
+				serializer.Serialize(textWriter, model);
+				Assert.That(textWriter.ToString(), Is.StringContaining("<MasterInstaller"), "Did not write out the MasterInstaller element");
+			}
+		}
+
+		[Test]
+		public void CanReadBasicModel()
+		{
+			var basicModelXmlString =
+				"<MasterInstaller><AutoConfigure/><General><Title>Test Installer</Title></General><Products><Product>" +
+				"<AutoConfigure><Title>Test</Title></AutoConfigure></Product></Products></MasterInstaller>";
+			var serializer = new XmlSerializer(typeof(ConfigurationModel));
+			using (var textReader = new StringReader(basicModelXmlString))
+			{
+				var model = (ConfigurationModel)serializer.Deserialize(textReader);
+				Assert.NotNull(model);
+				Assert.NotNull(model.AutoConfigure);
+				Assert.NotNull(model.General);
+				CollectionAssert.IsNotEmpty(model.Products);
+			}
+		}
+
+		[Test]
+		public void CanReadAutoConfigureSection()
+		{
+			var cppPath = @"F:\CD Builder\Master Installer";
+			var productsPath = @"F:\CD Builder\Products";
+			var cdPath = @"F:\CD Builder\CDs";
+			var helpSource = @"Documents\SILSoftwareSupport";
+			var helpDest = @"SILSoftwareSupport";
+			var TOUSource = @"Documents\TermsOfUse\License SIL Freeware.rtf";
+			var TOUDest = @"Docs";
+			var cdImagePath = @"F:\CD Builder\CDs\WordSurv 6";
+			var cdSize = @"700";
+			var minFileSize = @"2048";
+			var basicModelXmlString =
+				string.Format(@"<MasterInstaller><AutoConfigure>
+					<CppFilePath>{0}</CppFilePath>
+					<ProductsPath>{1}</ProductsPath>
+					<CDsPath>{2}</CDsPath>
+					<ExternalHelpSource>{3}</ExternalHelpSource>
+					<ExternalHelpDestination>{4}</ExternalHelpDestination>
+					<TermsOfUseSource>{5}</TermsOfUseSource>
+					<TermsOfUseDestination>{6}</TermsOfUseDestination>
+					<CdImagePath>{7}</CdImagePath>
+					<CdSize>{8}</CdSize>
+					<MinFileSize>{9}</MinFileSize>
+				</AutoConfigure></MasterInstaller>", cppPath, productsPath, cdPath, helpSource,
+															  helpDest, TOUSource, TOUDest, cdImagePath, cdSize, minFileSize);
+			var serializer = new XmlSerializer(typeof(ConfigurationModel));
+			using (var textReader = new StringReader(basicModelXmlString))
+			{
+				var model = (ConfigurationModel)serializer.Deserialize(textReader);
+				Assert.That(model.AutoConfigure.CppPath, Is.StringEnding(cppPath));
+				Assert.That(model.AutoConfigure.ProductsPath, Is.StringEnding(productsPath));
+				Assert.That(model.AutoConfigure.CdPath, Is.StringEnding(cdPath));
+				Assert.That(model.AutoConfigure.HelpSource, Is.StringEnding(helpSource));
+				Assert.That(model.AutoConfigure.HelpDest, Is.StringEnding(helpDest));
+				Assert.That(model.AutoConfigure.TermsSource, Is.StringEnding(TOUSource));
+				Assert.That(model.AutoConfigure.TermsDest, Is.StringEnding(TOUDest));
+				Assert.That(model.AutoConfigure.ImagePath, Is.StringEnding(cdImagePath));
+				Assert.That(model.AutoConfigure.CdSize, Is.EqualTo(int.Parse(cdSize)));
+				Assert.That(model.AutoConfigure.MinFileSize, Is.EqualTo(int.Parse(minFileSize)));
+			}
 		}
 	}
 }

405bbb332171fd0c4604dcb1746f245dd8608077
Jason Naylor <jason_naylor@sil.org>
2015-11-13 17:08:40 -0600

    Implement much of the ConfigProducts functionality
    * Modify model to add missing pieces used in ConfigProducts
    * Generate variables for the Product structs
    * Address minor review comment

--- Code and Projects/MasterInstallerConfigurator/MasterInstallerCompiler.cs
+++ Code and Projects/MasterInstallerConfigurator/MasterInstallerCompiler.cs
@@ -2,6 +2,7 @@
 // This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
 
 using System.IO;
+using System.Text;
 
 namespace MasterInstallerConfigurator
 {
@@ -62,6 +63,159 @@ namespace MasterInstallerConfigurator
 			File.WriteAllText(Path.Combine(cppFilePath, "ConfigDisks.cpp"), fileContents);
 		}
 
+		private static void GenerateProductsCpp(string cppFilePath, ConfigurationModel model)
+		{
+			var externalFunctions = new StringBuilder();
+			externalFunctions.AppendLine("// External functions:");
+			var productConfigurations = new StringBuilder();
+			productConfigurations.AppendLine("// List of software products in this package.");
+			productConfigurations.AppendLine("static SoftwareProduct Products[] =\n{");
+			var preInstallationDependencies = new StringBuilder();
+			preInstallationDependencies.AppendLine("// List of pre-installation dependencies.");
+			preInstallationDependencies.AppendLine("static const Mapping Prerequisites[] =\n{");
+			var postInstallationTests = new StringBuilder();
+			postInstallationTests.AppendLine("// List of post-installation dependencies.");
+			postInstallationTests.AppendLine("static const Mapping RunDependencies[] =\n{");
+			foreach (var product in model.Products)
+			{
+				GenerateProductExternalFunctions(product, externalFunctions);
+				productConfigurations.AppendLine("\t{");
+				GenerateProductStringVariable(product.Tag, "Name used in dependency lists", productConfigurations);
+				GenerateProductStringVariable(product.Title, "Title", productConfigurations);
+				GenerateProductStringVariable(product.ProductCode, "MSI Product Code", productConfigurations);
+				GenerateProductStringVariable(product.Version, "Version for Product Key test", productConfigurations);
+				GenerateProductIntVariable(product.KeyId, "-1", "Product Key ID", productConfigurations);
+				GenerateProductBoolVariable(product.List, "Flag saying if product should be offered to user (m_kOneOfOurs)",
+					productConfigurations);
+				// The IsContainer flag is obsolete and not present in any existing config files, but we will write it out as false for now
+				GenerateProductBoolVariable(false, "IsContainer (Obsolete, always false)",
+					productConfigurations);
+				GenerateProductStringVariable(null, "Critical file condition flag (Obsolete)", productConfigurations);
+				GenerateProductStringVariable(product.CriticalFile, "Critical file (was critical file condition true)", productConfigurations);
+				GenerateProductStringVariable(null, "Critical file condition false (Obsolete)", productConfigurations);
+				GenerateProductIntVariable(product.CDNumber, "CD index (zero-based)", productConfigurations);
+				GenerateProductStringVariable(null, "MinOS (Obsolete)", productConfigurations);
+				GenerateProductStringVariable(null, "MaxOS (Obsolete)", productConfigurations);
+				GenerateProductBoolVariable(product.MustBeAdmin, "Must be admin to install", productConfigurations);
+				GenerateProductBoolVariable(product.FlushReboot, "Flag to say if pending reboot must be executed first", productConfigurations);
+				GenerateProductBoolVariable(false, "flag related to reboot and PendingFileRenameOperations reg setting (Obsolete)", productConfigurations);
+				GenerateProductBoolVariable(false, "flag related to reboot and wininit.ini (Obsolete)", productConfigurations);
+				GenerateProductBoolVariable(false, "flag related to killing hanging windows before installation (Obsolete)", productConfigurations);
+				GenerateProductFunctionVariable(product.Preinstall, "Preinstallation function", productConfigurations);
+				GenerateProductStringVariable(null, "No External Preinstallation function (Obsolete)", productConfigurations);
+				GenerateProductBoolVariable(false, "Flag to say if we may ignore returned error code from Preinstall function (Obsolete)", productConfigurations);
+				GenerateProductInstallVariables(product, productConfigurations);
+				GenerateProductIntVariable(0, "Success code override (Obsolete)", productConfigurations);
+				GenerateProductBoolVariable(product.MustNotDelayReboot, "Flag to say if pending reboot must be executed right after installation", productConfigurations);
+				GenerateProductStringVariable(product.DownloadURL, "Download Web URL", productConfigurations);
+				GenerateProductStringVariable(product.Commentary, "Commentary during installation", productConfigurations);
+				GenerateProductStringVariable(product.StatusWindow, "What to do with status window during installation", productConfigurations);
+				GenerateProductBoolVariable(false, "StatusPauseWin98 flag (Obsolete)", productConfigurations);
+			}
+		}
+
+		private static void GenerateProductInstallVariables(ConfigurationModel.Product product, StringBuilder builder)
+		{
+			if (product.Install.Type == "Internal")
+			{
+				GenerateProductStringVariable(null, "No Install Cmd condition", builder);
+				GenerateProductStringVariable(null, "No Install Cmd for true condition", builder);
+				GenerateProductStringVariable(null, "No Install Cmd for false condition", builder);
+				GenerateProductStringVariable(null, "No MsiFlags", builder);
+				GenerateProductBoolVariable(false, "Don't test ANSI conversion", builder);
+				GenerateProductStringVariable(null, "No MsiUpgradeFrom", builder);
+				GenerateProductStringVariable(null, "No MsiUpgradeTo", builder);
+				GenerateProductStringVariable(null, "No MsiUpgradeFlags", builder);
+				GenerateProductFunctionVariable(product.Install.InstallFunction, "Internal install function", builder);
+			}
+			else
+			{
+				if (!string.IsNullOrEmpty(product.Install.Flag))
+				{
+					GenerateProductFunctionVariable(product.Install.Flag, "Install Cmd condition flag", builder);
+					GenerateProductPathVariable(product.Install.True, "Install Cmd condition true", builder);
+					GenerateProductPathVariable(product.Install.False, "Install Cmd condition true", builder);
+				}
+				else
+				{
+					GenerateProductStringVariable(null, "No Install Cmd condition", builder);
+					GenerateProductStringVariable(product.Install.InstallFunction, "External install Cmd", builder);
+					GenerateProductStringVariable(null, "No Install Cmd for false condition", builder);
+				}
+				GenerateProductStringVariable(product.Install.MsiFlags, "No MsiFlags", builder);
+				GenerateProductBoolVariable(false, "Don't test ANSI conversion (Obsolete)", builder);
+				GenerateProductStringVariable(product.Install.MsiUpgradeFrom, "MsiUpgradeFrom", builder);
+				GenerateProductStringVariable(product.Install.MsiUpgradeTo, "No MsiUpgradeTo", builder);
+				GenerateProductStringVariable(null, "No MsiUpgradeFlags (Obsolete)", builder);
+				GenerateProductFunctionVariable(null, "No internal install function", builder);
+			}
+		}
+
+		private static void GenerateProductPathVariable(string path, string comment, StringBuilder builder)
+		{
+			var pathValue = "NULL";
+			if (!string.IsNullOrEmpty(path))
+			{
+				pathValue = string.Format("_T(\"{0}\")", path.Replace("\\", "\\\\"));
+			}
+			builder.AppendLine(string.Format("{0}, // {1}", pathValue, comment));
+		}
+
+		private static void GenerateProductFunctionVariable(string preInstall, string comment, StringBuilder builder)
+		{
+			builder.AppendLine(string.Format("\t\t{0}, // {1}", string.IsNullOrEmpty(preInstall) ? "NULL" : preInstall, comment));
+		}
+
+		private static void GenerateProductIntVariable(int integerValue, string comment, StringBuilder productConfigurations)
+		{
+			GenerateProductIntVariable(integerValue.ToString(), "", comment, productConfigurations);
+		}
+
+		private static void GenerateProductBoolVariable(bool boolValue, string comment, StringBuilder productConfigurations)
+		{
+			productConfigurations.AppendLine(string.Format("\t\t{0}, {1}", boolValue.ToString().ToLower(), comment));
+		}
+
+		private static void GenerateProductIntVariable(string integerString, string valueIfEmpty, string comment, StringBuilder stringBuilder)
+		{
+			var intValue = valueIfEmpty;
+			if (!string.IsNullOrEmpty(integerString))
+				intValue = integerString;
+			stringBuilder.AppendLine(string.Format("\t\t{0}, {1}", intValue, comment));
+		}
+
+		private static void GenerateProductStringVariable(string variableValue, string comment, StringBuilder builder)
+		{
+			if (string.IsNullOrEmpty(variableValue))
+				variableValue = "NULL";
+			builder.AppendLine(string.Format("\t\t_T(\"{0}\"), // {1}", variableValue, comment));
+		}
+
+		private static void GenerateProductExternalFunctions(ConfigurationModel.Product product, StringBuilder externalFunctions)
+		{
+			if (product.TestPresence != null)
+			{
+				var productTestFunction =
+					string.Format("extern bool {0}(const _TCHAR * pszMinVersion, const _TCHAR * pszMaxVersion, SoftwareProduct * Product);",
+						product.TestPresence.TestFunction);
+				externalFunctions.AppendLine(productTestFunction);
+			}
+			if (!string.IsNullOrEmpty(product.Preinstall))
+			{
+				externalFunctions.AppendLine(string.Format("extern int {0}(SoftwareProduct * Product);", product.Preinstall));
+			}
+			if (product.Install != null && product.Install.Type == "Internal")
+			{
+				externalFunctions.AppendLine(string.Format("extern DWORD {0}(bool fFlag, SoftwareProduct * Product);",
+					product.Install.InstallFunction));
+			}
+			if (product.PostInstall != null)
+			{
+				externalFunctions.AppendLine(string.Format("extern int {0}(SoftwareProduct * Product);",
+					product.PostInstall.PostInstallFunction));
+			}
+		}
+
 		private static void StampCppString(string replaceString, string modelString, ref string fileContents)
 		{
 			var replacementText = "NULL";
@@ -85,7 +239,7 @@ namespace MasterInstallerConfigurator
 		private static string CreateTempCompilationFolder(ConfigurationModel model)
 		{
 			var folderRoot = Path.GetDirectoryName(model.FileLocation);
-			var tempFolder = Path.Combine(folderRoot, "setup.exe coming soon");
+			var tempFolder = Path.Combine(folderRoot, "setup.exeComingSoon");
 			Directory.CreateDirectory(tempFolder);
 			return tempFolder;
 		}
